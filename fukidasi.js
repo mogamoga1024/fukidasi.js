5a9a9c47c764c09b23c81e4fca226581ba94022e
mogamoga <mogamoga.g1thu6@gmail.com>
2023-09-03 03:27:42 +0900

    三角形 上と下

--- fukidasi.js
+++ fukidasi.js
@@ -31,17 +31,22 @@ const fukidasi = (function() {
 
         let strTriangleBase = "40px";
         let strTriangleHeight = "20px";
+        // let trianglePosition = "top";
+        // let trianglePosition = "bottom";
+        let trianglePosition = "left";
         if (options?.hasOwnProperty("triangleBase")) {
             strTriangleBase = options.triangleBase;
         }
         if (options?.hasOwnProperty("triangleHeight")) {
             strTriangleHeight = options.triangleHeight;
         }
+        if (options?.hasOwnProperty("trianglePosition")) {
+            trianglePosition = options.trianglePosition;
+        }
 
         const elFukidasi = document.createElement("div");
         const elTriangle = document.createElement("div");
         Object.assign(elFukidasi.style, {
-            top: `calc(${elTarget.clientHeight}px + ${strTriangleHeight} / 2)`,
             padding: "8px",
             textAlign: "center",
             borderRadius: "5px",
@@ -49,14 +54,22 @@ const fukidasi = (function() {
             backgroundColor: "black",
             userSelect: "none",
         });
+
+        if (trianglePosition === "top") {
+            elFukidasi.style.top = `calc(${elTarget.clientHeight}px + ${strTriangleHeight} / 2)`;
+        }
+        else if (trianglePosition === "left") {
+
+        }
+        else if (trianglePosition === "right") {
+
+        }
+        else if (trianglePosition === "bottom") {
+            elFukidasi.style.bottom = `calc(${elTarget.clientHeight}px + ${strTriangleHeight} / 2)`;
+        }
+
+        // カスタムCSSの反映
         Object.assign(elFukidasi.style, options);
-        Object.assign(elTriangle.style, {
-            position: "absolute",
-            top: `-${strTriangleHeight}`,
-            borderLeft: `calc(${strTriangleBase} / 2) solid transparent`,
-            borderRight: `calc(${strTriangleBase} / 2) solid transparent`,
-            borderBottom: `${strTriangleHeight} solid ${elFukidasi.style.backgroundColor}`,
-        });
         
         elFukidasi.innerText = text;
 
@@ -66,36 +79,57 @@ const fukidasi = (function() {
 
         const strWidth = getComputedStyle(elFukidasi).width;
         elFukidasi.style.width = strWidth;
-        elFukidasi.style.left = `calc((100% - ${elFukidasi.clientWidth}px) / 2)`;
-        elTriangle.style.left = `calc((${elFukidasi.clientWidth}px - ${strTriangleBase}) / 2)`;
+
+        if (trianglePosition === "top") {
+            elFukidasi.style.left = `calc((100% - ${elFukidasi.clientWidth}px) / 2)`;
+            Object.assign(elTriangle.style, {
+                position: "absolute",
+                top: `calc(-${strTriangleHeight} + 1px)`,
+                left: `calc((${elFukidasi.clientWidth}px - ${strTriangleBase}) / 2)`,
+                borderLeft: `calc(${strTriangleBase} / 2) solid transparent`,
+                borderRight: `calc(${strTriangleBase} / 2) solid transparent`,
+                borderBottom: `${strTriangleHeight} solid ${elFukidasi.style.backgroundColor}`,
+            });
+        }
+        else if (trianglePosition === "bottom") {
+            elFukidasi.style.left = `calc((100% - ${elFukidasi.clientWidth}px) / 2)`;
+            Object.assign(elTriangle.style, {
+                position: "absolute",
+                top: `calc(${elFukidasi.clientHeight}px - 1px)`,
+                left: `calc((${elFukidasi.clientWidth}px - ${strTriangleBase}) / 2)`,
+                borderLeft: `calc(${strTriangleBase} / 2) solid transparent`,
+                borderRight: `calc(${strTriangleBase} / 2) solid transparent`,
+                borderTop: `${strTriangleHeight} solid ${elFukidasi.style.backgroundColor}`,
+            });
+        }
 
         elTarget.removeChild(elFukidasi);
         elFukidasi.style.position = "absolute";
         elFukidasi.style.visibility = "";
 
-        const fadeOutDuration = 2000;
-        elFukidasi.animate([
-            {
-                visibility: "visible",
-                opacity: 1
-            },
-            {
-                visibility: "hidden",
-                opacity: 0
-            }
-        ], {
-            duration: fadeOutDuration,
-            easing: "ease-in",
-            fill: "forwards"
-        });
+        // const fadeOutDuration = 2000;
+        // elFukidasi.animate([
+        //     {
+        //         visibility: "visible",
+        //         opacity: 1
+        //     },
+        //     {
+        //         visibility: "hidden",
+        //         opacity: 0
+        //     }
+        // ], {
+        //     duration: fadeOutDuration,
+        //     easing: "ease-in",
+        //     fill: "forwards"
+        // });
 
         elTarget.appendChild(elFukidasi);
         elFukidasi.appendChild(elTriangle);
 
-        setTimeout(() => {
-            elTarget.removeChild(elFukidasi);
-            canExecuteMap.set(elTarget, true);
-        }, fadeOutDuration);
+        // setTimeout(() => {
+        //     elTarget.removeChild(elFukidasi);
+        //     canExecuteMap.set(elTarget, true);
+        // }, fadeOutDuration);
     }
 })();
 
