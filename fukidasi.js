23f81d9bf7a3e090d049160639ac7aa322ee52f2
mogamoga <mogamoga.g1thu6@gmail.com>
2023-09-02 18:25:39 +0900

    きらめいてピース はじまるよ

--- fukidasi.js
+++ fukidasi.js
@@ -1,8 +1,8 @@
 
 /**
- * targetの吹き出しを表示する。
- * targetはposition: relativeであること。
- * @param {HTMLElement} target 吹き出しの対象
+ * elTargetの吹き出しを表示する。
+ * elTargetはposition: relativeであること。
+ * @param {HTMLElement} elTarget 吹き出しの対象
  * @param {string} text 表示する文字列
  */
 const fukidasi = (function() {
@@ -11,10 +11,9 @@ const fukidasi = (function() {
     const elFukidasi = document.createElement("div");
     Object.assign(elFukidasi.style, {
         position: "absolute",
-        // width: "100px",
+        display: "inline-block",
         height: "40px",
         top: "60px",
-        left: "calc((100% - 100px) / 2)",
         fontSize: "1.2rem",
         lineHeight: "40px",
         textAlign: "center",
@@ -25,10 +24,10 @@ const fukidasi = (function() {
     });
 
     /**
-     * @param {HTMLElement} target
+     * @param {HTMLElement} elTarget
      * @param {string} text
      */
-    return function(target, text) {
+    return function(elTarget, text) {
         if (!canExecute) {
             return;
         }
@@ -38,7 +37,18 @@ const fukidasi = (function() {
 
         elFukidasi.innerText = text;
 
-        target.appendChild(elFukidasi);
+        elFukidasi.style.visibility = "hidden";
+        document.body.appendChild(elFukidasi);
+
+        // debugger
+        elFukidasi.style.width = `${elFukidasi.clientWidth}px`;
+        elFukidasi.style.padding = "0 16px";
+        elFukidasi.style.left = `calc((100% - ${elFukidasi.clientWidth}px) / 2)`;
+
+        document.body.removeChild(elFukidasi);
+        elFukidasi.style.visibility = "";
+
+        elTarget.appendChild(elFukidasi);
     }
 })();
 
